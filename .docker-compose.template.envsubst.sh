f15c30c93213f6f5fd4408408a7b1ffcf989e6be
Kaj Magnus Lindberg <kajmagnus3@gmail.com>
2019-01-14 01:43:39 +0000

    Add docker-compose.yml and a template and script.

---
+++ .docker-compose.template.envsubst.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# This creates a file  docker-compose.yml  that looks like  .docker-compose.template.yml,
+# except that there're default values for DOCKER_REPOSITORY and TALKYARD_VERSION_TAG.
+# This is a simple way to update the default Talkyard version, for all images,
+# once a new Talkyard version has been released.
+
+VERSION=`tail -n1 version-tags.log`
+
+DOCKER_REPOSITORY='${DOCKER_REPOSITORY:-debiki}' \
+   TALKYARD_VERSION_TAG='${TALKYARD_VERSION_TAG:-'"$VERSION"'}' \
+   envsubst '${DOCKER_REPOSITORY} ${TALKYARD_VERSION_TAG}' \
+     < .docker-compose.template.yml \
+     > docker-compose.yml
