2ddd9a47cbf507818eed8589b55c28d9e6ebb851
Kaj Magnus Lindberg <kajmagnus3@gmail.com>
2020-02-09 12:21:46 +0100

    Bump docker-compose.yml to v0.6.51-163f5bd
    And edit bump script: Ignore WIP versions.

--- .docker-compose.template.envsubst.sh
+++ .docker-compose.template.envsubst.sh
@@ -5,10 +5,17 @@
 # This is a simple way to update the default Talkyard version, for all images,
 # once a new Talkyard version has been released.
 
-VERSION=`tail -n1 version-tags.log`
+VERSION=$(grep -iv WIP version-tags.log | tail -n1)
+
+echo
+echo "Bumping Talkyard version in docker-compose.yml to $VERSION ..."
 
 DOCKER_REPOSITORY='${DOCKER_REPOSITORY:-debiki}' \
    TALKYARD_VERSION_TAG='${TALKYARD_VERSION_TAG:-'"$VERSION"'}' \
    envsubst '${DOCKER_REPOSITORY} ${TALKYARD_VERSION_TAG}' \
      < .docker-compose.template.yml \
      > docker-compose.yml
+
+echo "Done."
+echo
+
